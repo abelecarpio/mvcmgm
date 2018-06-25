decd8e12927ddf512c36eb2c7194d1c9e396272e
abelecarpio <abelecarpio@live.com.ph>
2018-06-25 22:14:17 +0800

    commit local

---
+++ project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mvcmgm.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
